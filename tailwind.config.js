dc8f12df07e48cb11191cc56d3baa1da50ba77a7
nurmuhammadrizky <nrmrizky5@gmail.com>
2023-07-21 18:49:43 +0700

    make single price card with frontend mentor

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        cyan: "hsl(179, 62%, 43%)",
+        brightYellow: "hsl(71, 73%, 54%)",
+        lightGray: "hsl(204, 43%, 93%)",
+        grayishBlue: "hsl(218, 22%, 67%)",
+      },
+    },
+  },
+  plugins: [],
+};
